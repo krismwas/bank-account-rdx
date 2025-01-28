b6c7059f24a49231d2de5950c5021dafa1429196
kris <krismwasgit@gmail.com>
2025-01-28 16:16:48 +0300

    configured store to modern toolkit

--- src/store.js
+++ src/store.js
@@ -1,17 +1,10 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducer = combineReducers({
-  customer: customerReducer,
-  account: accountReducer,
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: { customer: customerReducer, account: accountReducer },
 });
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
